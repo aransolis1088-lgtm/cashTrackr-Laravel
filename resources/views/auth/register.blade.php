c6689cc7967c3a11bbb18367a4b7012a257dbb2d
Aran Uzziel Solis Medina <aran.solis1088@gmail.com>
2026-05-31 20:57:20 -0600

    feat: Agregar vistas de autenticación y layouts

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.auth')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title')
+    Crear Cuenta
+@endsection
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    @fonts
-
-    <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
-</head>
-
-<body>
+@section('auth-content')
     <form class="mt-14 space-y-5" novalidate>
         <div class="space-y-2">
             <label class="font-bold text-2xl block" for="name">Nombre</label>
@@ -34,18 +23,18 @@
         <div class="space-y-2">
             <label class="font-bold text-2xl block">Password</label>
 
-            <input type="password" placeholder="Password de Registro"
-                class="w-full border border-gray-300 p-3 rounded-lg" name="password" />
+            <input type="password" placeholder="Password de Registro" class="w-full border border-gray-300 p-3 rounded-lg"
+                name="password" />
         </div>
 
         <div class="space-y-2">
             <label class="font-bold text-2xl block" for="password_confirmation">Repetir Password</label>
 
-            <input type="password" placeholder="Password de Registro"
-                class="w-full border border-gray-300 p-3 rounded-lg" name="password_confirmation" />
+            <input type="password" placeholder="Password de Registro" class="w-full border border-gray-300 p-3 rounded-lg"
+                name="password_confirmation" />
         </div>
 
         <input type="submit" value='Registrarme'
             class="bg-purple-950 hover:bg-purple-800 w-full p-3 rounded-lg text-white font-bold  text-xl cursor-pointer" />
     </form>
-</body>
+@endsection
