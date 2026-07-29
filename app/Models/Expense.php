08a10ff9b878e8f574b63a7b8f1a2954f0afb3c3
aransolis1088-lgtm <aran.solis1088@gmail.com>
2026-07-28 21:56:01 -0600

    feat - Testing creación de gastos

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use App\ExpenseCategory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[Fillable(['name', 'amount', 'category', 'budget_id'])]
 class Expense extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $casts = [
         'category' => ExpenseCategory::class,
